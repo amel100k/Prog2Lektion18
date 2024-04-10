66cec66c7ef3f94f2341b8765f1451dbfc6fefff
benn-christensen <bnch@eaaa.dk>
2024-04-10 12:42:24 +0200

    Initial commit

---
+++ src/opgave03/Opgave03.java
@@ -0,0 +1,7 @@
+package opgave03;
+
+public class Opgave03 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tests/examples/models/CircularArrayQueueTest.java
@@ -0,0 +1,93 @@
+package examples.models;
+
+import opgave01.models.Queue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularArrayQueueTest {
+    private Queue<String> sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new CircularArrayQueue<>(2);
+    }
+
+    @Test
+    @DisplayName("Add element to queue")
+    void add() {
+        int size = sut.size();
+        sut.add("Test1");
+        assertEquals(size + 1, sut.size());
+        assertEquals("Test1", sut.element());
+        sut.add("Test2");
+        sut.remove();
+        sut.add("Test3"); //circular test
+        assertEquals(size + 2, sut.size());
+        assertEquals("Test2", sut.element());
+        sut.add("Test4"); //Adding elements over initial capacity
+        assertEquals(size + 3, sut.size());
+        assertEquals("Test2", sut.element());
+    }
+
+
+    @Test
+    @DisplayName("Remove element from Queue")
+    void remove() {
+        sut.add("Test1");
+        sut.add("Test2");
+
+        String actual = sut.remove();
+        assertEquals("Test1", actual);
+        assertEquals(1, sut.size());
+    }
+
+    @Test
+    @DisplayName("Remove element from empty queue")
+    void removeFromEmptyQueue() {
+        Exception actual = assertThrows(NoSuchElementException.class, () -> sut.remove());
+        assertEquals("Queue is empty", actual.getMessage());
+    }
+
+    @Test
+    @DisplayName("element from empty queue")
+    void elementFromEmptyQueue() {
+        Exception actual = assertThrows(NoSuchElementException.class, () -> sut.element());
+        assertEquals("Queue is empty", actual.getMessage());
+    }
+
+    @Test
+    @DisplayName("isEmpty")
+    void isEmpty() {
+        boolean actual = sut.isEmpty();
+        assertTrue(actual);
+        sut.add("Test");
+        actual = sut.isEmpty();
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("clear()")
+    void clear() {
+        sut.add("Test");
+        sut.clear();
+        assertEquals(0, sut.size());
+        assertThrows(NoSuchElementException.class, () -> sut.element());
+    }
+
+    @Test
+    void size() {
+        int actual = sut.size();
+        assertEquals(0, actual);
+        sut.add("Test");
+        actual = sut.size();
+        assertEquals(1, actual);
+        sut.remove();
+        actual = sut.size();
+        assertEquals(0, actual);
+    }
+}
